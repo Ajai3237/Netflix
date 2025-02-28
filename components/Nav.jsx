d6ea4f20d48623a23d5b9aea4df596a9c3941c7e
ajai krishna m <ajai12346aji@gmail.com>
2025-02-28 11:41:09 +0530

    rdit

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,7 +4,7 @@ function Nav() {
   return (
     <>
       <div className="">
-        <img className='Nav' style={{width:'150px' }} src="/src/img/png-transparent-netflix-logo-netflix-television-show-streaming-media-film-netflix-logo-television-text-trademark-thumbnail-removebg-preview.png" alt="" />
+        <img className='Nav' style={{width:'150px' }} src="img/png-transparent-netflix-logo-netflix-television-show-streaming-media-film-netflix-logo-television-text-trademark-thumbnail-removebg-preview.png" alt="" />
       </div>
     </>
   )
